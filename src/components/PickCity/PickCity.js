8cc72aebe86b1f3674873309486adcf13037596f
Monika Grzanek <grzanekmonika@o2.pl>
2024-06-07 08:23:33 +0200

    Add handleCityChange function in WeatherBox component and add handleSubmit function in PickCity component

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -7,12 +7,18 @@ import { useState } from 'react';
 const PickCity = ({action}) => {
   const [city, setCity] = useState('');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    action(city);
+    setCity('');
+  }
+
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}> 
       <label>
         <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
       </label>
-      <Button>Search</Button>
+      <Button type="submit">Search</Button>
     </form>
   );
 };
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,15 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+  const handleCityChange = useCallback(valueCity => {
+    console.log(valueCity);
+  }, []);
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>

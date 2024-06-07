156a010ba2e1dc05e14bcd26d0a697a430161355
Monika Grzanek <grzanekmonika@o2.pl>
2024-06-07 13:56:26 +0200

    Add fetch to get current weather data for selected city in WeatherBox component

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -4,9 +4,25 @@ import Loader from '../Loader/Loader';
 import { useCallback } from 'react';
 
 const WeatherBox = props => {
-  const handleCityChange = useCallback(valueCity => {
-    console.log(valueCity);
+  const handleCityChange = useCallback(valueCity => {   
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${valueCity}&appid=5d9df579968077defb121d007ddd656a&units=metric`)
+   .then(res => res.json())
+   .then(data => {
+     console.log(data);
+
+     const weatherData = {
+      city: data.name,
+      temp: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].main
+};
+
+   });
+   
   }, []);
+
+  
+
   return (
     <section>
       <PickCity action={handleCityChange} />

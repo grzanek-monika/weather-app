ba70aa0b8c07c64783840323cf88d0609cd8cf42
Monika Grzanek <grzanekmonika@o2.pl>
2024-06-07 14:25:26 +0200

    Add weatherDate to useState in WeatherBox and send it to child WeatherSummary component

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,21 +1,24 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+const [weatherData, setWeatherData] = useState(null);
   const handleCityChange = useCallback(valueCity => {   
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${valueCity}&appid=5d9df579968077defb121d007ddd656a&units=metric`)
-   .then(res => res.json())
-   .then(data => {
-     console.log(data);
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
 
-     const weatherData = {
+     setWeatherData({
       city: data.name,
       temp: data.main.temp,
       icon: data.weather[0].icon,
       description: data.weather[0].main
-};
+    });
+
+    console.log('summary:', weatherData);
 
    });
    
@@ -26,7 +29,7 @@ const WeatherBox = props => {
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      <WeatherSummary {...weatherData}  />
       <Loader />
     </section>
   )
